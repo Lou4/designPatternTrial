3a22c50a771330ab8cd0bb7a52876b76cfe3a984
Lou4 <tagliabue.luca@gmail.com>
2016-04-25 21:56:54 +0200

    add test with missing implementation

--- MVC/src/test/java/com/model/TestModel.java
+++ MVC/src/test/java/com/model/TestModel.java
@@ -1,5 +1,22 @@
 package com.model;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestModel {
 
+	private TemperatureModel temperatureModel;
+	
+	@Before
+	public void setUp(){
+		temperatureModel = new TemperatureModel();
+	}
+	
+	@Test
+	public void testModel_init(){
+		temperatureModel.setTemperature(10);
+		assertEquals(10, temperatureModel.getTemperature());
+	}
+	
 }
