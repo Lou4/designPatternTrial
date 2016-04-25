f6b134b791bd8f3ce85b57bbe82a6fe7bcbe0445
Marco Predari <predorock@gmail.com>
2016-04-25 21:57:01 +0200

    Forgot some files

---
+++ MVC/src/main/java/com/model/TemperatureModel.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class TemperatureModel {
+
+    private Integer temperature;
+
+    public TemperatureModel(){
+        this.temperature = 0;
+    }
+
+    public Integer getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Integer temperature) {
+        this.temperature = temperature;
+    }
+}
